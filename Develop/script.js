4a989c013702b15eb0fb177dc144c8fb99a63514
alex <alexreveles@yahooo.com>
2021-01-17 11:16:39 -0800

    setting and retrieving localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,45 +1,67 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-
-
-
-
-
-
+// when page loads we need to retrieve the local storage
+// when the page loads we need to be aware of the local date/time
 $(window).on("load", function () {
  
+    // use moment to get the date/time in nice format
     var currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
     $("#currentDay").append(currentDate);
+
+    // we can't work with partial time i.e. 10.05am, so need to get
+    // it in exact rounded format, so we get the H only
     var currentTime = moment().format("H");
     
+    // calling the fields by whole time, ie. 9, 10, 11 in order
+    // to make it easier to loop around it
+
+    // loop through all the times to determine if past, present, future
+    // use existing CSS to define what they are
     for(i=9; i<= 17; i++) {
 
         var CurrentContainer = i;
+
+        // if ID of the Div matches the time then its CURRENT
         if (currentTime == i) {
+            // set the DIV ID 
             $('#' + CurrentContainer).addClass("present");
+
+            // set the TEXTAREA to the right color
             $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("present");
         }
+
+        // if ID of the Div is greater than the time then its past
         else if (currentTime > i) {
             $('#' + CurrentContainer).addClass("past");
             $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("past");
         }
+
+        // if ID of the Div is less than the time then its past
         else if (currentTime < i) {
             $('#' + CurrentContainer).addClass("future");
             $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("future");
         }
 
+    }
+
+
+    $("button").on("click", function() {
+
+        //if the button for save is clicked, save the entirety
+        //of all fields into local storage, instead of one-by-one
+        
+        for(i=9; i<= 17; i++) {
+
+            var CurrentContainer = i;
+            localStorage.setItem(i + "am", ($('#' + i).children('div').children('div').children("textarea").val()));
+
+        }
+
+    })
+
+    // retrieve from local storage and append it to the textarea
+    for(i=9; i<= 17; i++) {
+
+        var CurrentContainer = i;
+        $('#' + i).children('div').children('div').children("textarea").append(localStorage.getItem(i + "am"));
     }
 
 })
